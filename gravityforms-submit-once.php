6c346a6e0f1b05d6a991ff1db0545e728af0346c
Laurens Offereins <lmoffereins@gmail.com>
2014-11-15 20:54:25 +0100

    Initial commit

---
+++ gravityforms-submit-once.php
@@ -0,0 +1,166 @@
+<?php
+
+/**
+ * The Gravity Forms Submit Once Plugin
+ * 
+ * @package Gravity Forms Submit Once
+ * @subpackage Main
+ */
+
+/**
+ * Plugin Name: Gravity Forms Submit Once
+ * Description: Limit Gravity Form forms to accept only one entry per user.
+ * Plugin URI:  https://github.com/lmoffereins/gravityforms-submit-once/
+ * Version:     1.0.0
+ * Author:      Laurens Offereins
+ * Author URI:  https://github.com/lmoffereins/
+ * Text Domain: gravityforms-submit-once
+ * Domain Path: /languages/
+ */
+
+// Exit if accessed directly
+defined( 'ABSPATH' ) || exit;
+
+if ( ! class_exists( 'GravityForms_Submit_Once' ) ) :
+/**
+ * The main plugin class
+ *
+ * @since 1.0.0
+ */
+final class GravityForms_Submit_Once {
+
+	/**
+	 * The plugin meta field key
+	 *
+	 * @since 1.0.0
+	 * @var string
+	 */
+	private $meta_key = 'gform-submit-once';
+
+	/**
+	 * Setup and return the singleton pattern
+	 *
+	 * @since 1.0.0
+	 *
+	 * @uses GravityForms_Submit_Once::setup_globals()
+	 * @uses GravityForms_Submit_Once::setup_actions()
+	 * @return The single GravityForms_Submit_Once
+	 */
+	public static function instance() {
+
+		// Store instance locally
+		static $instance = null;
+
+		if ( null === $instance ) {
+			$instance = new GravityForms_Submit_Once;
+			$instance->setup_actions();
+		}
+
+		return $instance;
+	}
+
+	/**
+	 * Prevent the plugin class from being loaded more than once
+	 */
+	private function __construct() { /* Nothing to do */ }
+
+	/** Private methods *************************************************/
+
+	/**
+	 * Setup default actions and filters
+	 *
+	 * @since 1.0.0
+	 */
+	private function setup_actions() {
+
+		// Displaying form
+		add_filter( '', array( $this, 'bail_submitted_form' ), 10, 2 );
+
+		// Admin settings
+		add_filter( '', array( $this, 'register_form_meta'   ) );
+		add_action( '', array( $this, 'display_form_setting' ) );
+	}
+
+	/** Public methods **************************************************/
+
+	/**
+	 * Do not display the form when the current user already submitted once
+	 *
+	 * @since 1.0.0
+	 *
+	 * @uses get_current_user_id()
+	 * @uses apply_filters() Calls 'gf_submit_once_bail_form'
+	 * 
+	 * @param bool $display Whether to display the form
+	 * @param int $form_id The form ID
+	 * @return bool Whether to continue displaying the form
+	 */
+	public function bail_submitted_form( $display = true, $form_id = 0 ) {
+
+		// Get the current user
+		$user_id = get_current_user_id();
+
+		// Form is marked to allow submissions once
+		if ( gform_get_form_meta( $form_id, $this->meta_key ) ) {
+
+			// Get whether user has submitted to this form yet
+			$display = ! (bool) gform_get_user_form_entries( $form_id, $user_id );
+		}
+
+		return apply_filters( 'gf_submit_once_bail_form', $display, $form_id, $user_id );
+	}
+
+	/** Admin Settings **************************************************/
+
+	/**
+	 * Register plugin form meta field
+	 *
+	 * GF also updates our meta field by this registration. So the meta field key
+	 * should correspond with the name attribute of the settings input field.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param array $form_meta Form meta fields
+	 * @return array Form meta fields
+	 */
+	public function register_form_meta( $form_meta = array() ) {
+
+		// Append our meta field
+		$form_meta[ $this->meta_key ] = __( 'Limit form entries', 'gravityforms-submit-once' );
+
+		return $form_meta;
+	}
+
+	/**
+	 * Display the plugin form setting
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param int $form_id Form ID
+	 */
+	public function display_form_setting( $form_id ) { ?>
+
+		<label>
+			<input type="checkbox" name="submit-once" value="1" <?php checked( gform_get_form_meta( $form_id, $this->meta_key ) ); ?>>
+			<span class="description"><?php _e( 'Limit this form to accept only one entry per user.', 'gravityforms-submit-once' ); ?></span>
+		</label>
+
+		<?php
+	}
+}
+
+/**
+ * Return single instance of this main plugin class
+ *
+ * @since 1.0.0
+ * 
+ * @return GravityForms_Submit_Once
+ */
+function gravityforms_submit_once() {
+	return GravityForms_Submit_Once::instance();
+}
+
+// Initiation depends on GF
+add_action( 'gform_init', 'gravityforms_submit_once' );
+
+endif; // class_exists
---
+++ readme.txt
@@ -0,0 +1,19 @@
+=== GF Submit Once ===
+Contributors: Offereins
+Tags: Gravity Forms, submit, submission, once, single, reply, entry
+Requires at least: 4.0
+Tested up to: 4.0
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Limit Gravity Form forms to accept only one entry per user.
+
+== Description ==
+
+This plugin introduces a Gravity Forms form setting to mark it to accept only one entry per user.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
